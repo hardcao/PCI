75d32082edda5b4182837cc04bf317c99a001808
曹小毛 <xiaomao.cao@renren-inc.com>
2014-04-09 16:53:11 +0800

    4-9.1版
    在以往中F1最好的结果的基层上进行优化

--- example/getPfile.cpp
+++ example/getPfile.cpp
@@ -36,14 +36,14 @@ struct B2B{
 	int cnt;
 }Buy[CN],coll[CN];
 double num[CN];
-int vist[CN],print[CN],vist1[CN],mutl[CN];
+int vist[CN],print[CN];
 double getSim(Person p1,Person p2){
 	Commodity cd[N];
 	int n =0;
 	double sum1=0,sum2=0,sum1Sq =0,sum2Sq =0,pSum =0;
 	for(int i=0;i<p1.ln;i++){
 		for (int j=0;j<p2.ln;j++){
-			if(p1.cd[i].cmd == p2.cd[j].cmd){
+			if(p1.cd[i].cmd == p2.cd[j].cmd&&p1.cd[i].dt == p2.cd[j].dt){
 				int sc1 = p1.cd[i].sc;
 				int sc2 = p2.cd[j].sc;
 				if(p1.cd[i].dt == p2.cd[j].dt){
@@ -68,12 +68,11 @@ double getSim(Person p1,Person p2){
 	}
 	return num/den;
 }
+
 void setMutlBuy(int lgn){
 	for(int i=0;i<lgn;i++){
 		int cnt =0;
-		if(lg[i].sc != 1) continue;
 		for(int j=i+1;j<lgn;j++){
-			if(lg[j].sc != 1) continue;
 			if(lg[i].pn==lg[j].pn&&lg[i].cmd==lg[j].cmd){
 				cnt++;
 			}
@@ -81,6 +80,7 @@ void setMutlBuy(int lgn){
 		if(cnt) vist[lg[i].cmd]=1;
 	}
 }
+
 void setB2B(int lgn){
 	for(int i=0;i<CN;i++){Buy[i].ln =0,Buy[i].rcm=-1;}
 	for(int i=0;i<lgn;i++){
@@ -185,8 +185,8 @@ int main()
 		dt = lg[i].dt;
 		// if(dt>195) continue;
 		if(sc == 1) sc = 4;
-		if(sc == 2) sc = 3;
-		if(sc == 3) sc = 2;
+		if(sc == 2) sc = 2;
+		if(sc == 3) sc = 1;
 		int flg = 0;
 		for(int j=0;j<lt;j++){
 			if(pnt[j].pn == pn){
@@ -194,7 +194,7 @@ int main()
 				int idx = pnt[j].ln;
 				int vt =1;
 				for (int t = 0;t<pnt[j].ln;t++){
-					if(pnt[j].cd[t].cmd == cmd&&vt){
+					if(pnt[j].cd[t].cmd == cmd&&pnt[j].cd[t].dt==dt&&vt){
 						vt = 0;
 						pnt[j].cd[t].sc+=sc;
 					}
@@ -219,61 +219,64 @@ int main()
 	int flgg =0;
 	for(int i=0;i<CN;i++) {vist[i]=0;}
 	setMutlBuy(lgn);
-	setB2B(lgn);
 	setC2B(lgn);
+	setB2B(lgn);
     for(int t=0;t<lt;t++){
-	for(int i=0;i<CN;i++) {num[i]=0;vist1[i]=0;mutl[i]= 0;}
+	for(int i=0;i<CN;i++) {num[i]=0;}
 	for(int i=1;i<lt;i++){
 		if(i == t) continue;
 		double ratio = getSim(pnt[t],pnt[i]);
-		// if(ratio>0.5)	printf("ratio==%f\n",ratio);
 		for(int j=0;j<pnt[i].ln;j++){
 			int idx = pnt[i].cd[j].cmd;
 			num[idx] += ratio*pnt[i].cd[j].sc;
 		}
 	}
 	int cnt =0;
-	int bmax = -1,bestb = -1;
+	for (int i=0;i<CN;i++) print[i]=0;
 	for(int i=0;i<pnt[t].ln;i++){
 		int idx = pnt[t].cd[i].cmd;
-		if(pnt[t].cd[i].sc>=9){
-			mutl[idx] =1;
+		if(int(num[idx])>3||vist[idx]){
+			print[idx] =1;
 		}
-		if(num[idx]>3){
-			vist1[idx]=1;
+	}
+	for(int i=0;i<pnt[t].ln;i++){
+		int idx = pnt[t].cd[i].cmd;
+		if(pnt[t].cd[i].sc > 9){
+			if(Buy[idx].rcm !=-1){
+				print[Buy[idx].rcm]=1;
+			}
 		}
 	}
 	for(int i=0;i<lgn;i++){
-		if(pnt[t].pn==lg[i].pn){
-			int idx = lg[i].cmd;
-			if(lg[i].sc == 1){
-				if(Buy[idx].rcm !=-1&&mutl[idx]) vist1[Buy[idx].rcm]=1;
-				if(vist[idx]){
-					vist1[idx]=1;
-				}
-			}
-			if(lg[i].sc == 2){
-				if(coll[idx].rcm != -1) {vist1[coll[idx].rcm]=1;}
+		int idx = lg[i].cmd;
+		if(lg[i].sc == 2 && lg[i].pn == pnt[t].pn){
+			if(coll[lg[i].cmd].rcm!=-1){
+				print[coll[idx].rcm] =1;
 			}
 		}
+		if(lg[i].sc == 1 && lg[i].pn == pnt[t].pn){
+			if(vist[idx]) print[idx] =1;
+		}
 	}
-	int cn =0;
-	for(int i=0;i<CN;i++){if(vist1[i]) cn++;}
-	if(cn ==0) continue;
-	int flg = 0;
-	printf("%d  ",pnt[t].pn);
 	for(int i=0;i<CN;i++){
-		if(vist1[i]){
-			if(flg == 0){
-				flg ++;
-			} else {
-				printf(",");
-			}
-			printf("%d",i);
-			// printf("%d %d\n",pnt[t].pn,i);
+		cnt+=print[i];
+	}
+	if(cnt==0) continue;
+	// printf("%d  ",pnt[t].pn);
+	int flg =0;
+	for(int i=0;i<CN;i++){
+		if(!print[i]) continue;
+		if(print[i]){
+			// if(flg == 0){
+// 				flg ++;
+// 			} else {
+// 				printf(",");
+// 			}
+// 			printf("%d",i);
+			printf("%d %d\n",pnt[t].pn,i);
 		}
 	}
-	 printf("\n");
+	 // printf("\n");
 	}
 	// printf("%d\n",flgg);
 	return 0;
